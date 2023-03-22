dbc9dca6f0a2cb37027261ce25b9dd5b76e56913
mzastsenski <koljan_z@yahoo.de>
2023-03-22 09:48:45 +0100

    headerForm useCallback added

--- frontend/src/components/HeaderForm/index.jsx
+++ frontend/src/components/HeaderForm/index.jsx
@@ -1,41 +1,42 @@
-import React, { useState, useMemo, useEffect } from 'react';
-import { categories } from '../../data/categories';
-import Input from '../../UI/Input';
-import debouce from 'lodash.debounce';
-import s from './index.module.scss';
+import React, { useState, useMemo, useEffect, useCallback } from "react";
+import { categories } from "../../data/categories";
+import Input from "../../UI/Input";
+import debounce from "lodash.debounce";
+import s from "./index.module.scss";
 
 export default function HeaderForm() {
-	const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
-	let listToDisplay = categories;
+  let listToDisplay = categories;
 
-	const handleChange = (el) => {
-		setSearchTerm(el.target.value);
-	};
+  const handleChange = useCallback((el) => {
+    setSearchTerm(el.target.value);
+  }, []);
 
-	const renderVideoList = () => {
-		return listToDisplay.map((category, i) => <p key={i}>{category}</p>);
-	};
+  const renderVideoList = () => {
+    return listToDisplay.map((category, i) => <p key={i}>{category}</p>);
+  };
 
-	if (searchTerm !== '') {
-		listToDisplay = categories.filter((category) => {
-			return category.includes(searchTerm);
-		});
-	}
+  if (searchTerm !== "") {
+    listToDisplay = categories.filter((category) => {
+      return category.includes(searchTerm);
+    });
+  }
 
-	const debouncedResults = useMemo(() => {
-		return debouce(handleChange, 200);
-	}, []);
+  const debouncedResults = useMemo(() => {
+    return debounce(handleChange, 200);
+  }, [handleChange]);
 
-	useEffect(() => {
-		return () => {
-			debouncedResults.cancel();
-		};
-	});
-	return (
-		<form className={s.form}>
-			<Input type="text" onChange={debouncedResults} />
-			{renderVideoList()}
-		</form>
-	);
+  useEffect(() => {
+    return () => {
+      debouncedResults.cancel();
+    };
+  }, [debouncedResults]);
+
+  return (
+    <form className={s.form}>
+      <Input type="text" onChange={debouncedResults} />
+      {renderVideoList()}
+    </form>
+  );
 }
