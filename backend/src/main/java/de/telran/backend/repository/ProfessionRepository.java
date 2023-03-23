6a36993f3451d51111eca27e7e9c755656464dde
Ivan Zamula <ivan.zamula@gmail.com>
2023-03-23 23:40:56 +0300

    add ignore case search for profession and video, add search not only for titles but also for profession

--- backend/src/main/java/de/telran/backend/repository/ProfessionRepository.java
+++ backend/src/main/java/de/telran/backend/repository/ProfessionRepository.java
@@ -1,11 +1,17 @@
 package de.telran.backend.repository;
 
 import de.telran.backend.entity.Profession;
+import de.telran.backend.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Set;
 
 public interface ProfessionRepository extends JpaRepository<Profession, Long> {
-    List<Profession> findByNameContains(String name);
+    List<Profession> findByNameContainsIgnoreCase(String name);
+
+
+
 }
