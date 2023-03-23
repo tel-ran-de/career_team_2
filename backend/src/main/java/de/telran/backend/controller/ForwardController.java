678f5ede3e8722717887b4dd93063e2696289ad7
Anna <anna.piskunova@web.de>
2023-03-23 21:42:54 +0100

    doc

--- backend/src/main/java/de/telran/backend/controller/ForwardController.java
+++ backend/src/main/java/de/telran/backend/controller/ForwardController.java
@@ -4,11 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/**
+ * Class describes the controller that returns index.html
+ * @author Ivan Zamula, Anna Etzler
+ * @version 1.0
+ */
 @Controller
 public class ForwardController {
     private static final Logger log = LoggerFactory.getLogger(VideoController.class);
-
+    /**
+     * Method  describes the controller that returns index.html
+     * @author Ivan Zamula, Anna Etzler
+     * @version 1.0
+     * @return index.html
+     */
     @RequestMapping("/")
     public String index() {
         log.info("Forward to index.html");
